7b76a8d1c7fdd0c0db3a99d362e223a92d7a2608
dsdunn <dsdunnmusic@gmail.com>
2020-04-25 20:58:28 -0600

    Start mock gpio module

--- setup_database.sh
+++ setup_database.sh
@@ -7,3 +7,15 @@ echo "SELECT 'CREATE USER pi' WHERE NOT EXISTS (SELECT FROM pg_database WHERE da
 psql -U postgres -c "alter role pi with password 'raspberry';"
 
 psql -U postgres -d sprinkler -c "grant all privileges on database sprinkler to pi;"
+
+psql -U postgres -d sprinkler -c "create schema if not exists sprinkler;"
+
+psql -U postgres -d sprinkler -c "create table if not exists sprinkler.schedules (i integer);"
+
+psql -U postgres -d sprinkler -c "
+CREATE TABLE IF NOT EXISTS schedules (
+  username varchar(45) NOT NULL,
+  password varchar(450) NOT NULL,
+  enabled integer NOT NULL DEFAULT '1',
+  PRIMARY KEY (username)
+)"
